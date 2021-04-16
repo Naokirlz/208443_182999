0bb7860a01485e1fa80060723928214998392a1f
Naokirlz <cristian_pd88@hotmail.com>
2021-04-16 20:58:59 -0300

    prueba nombre categoría

---
+++ Obligatorio/Negocio/Clases/Categoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Negocio.Excepciones;
+
+namespace Negocio
+{
+    public class Categoria
+    {
+        public string Nombre { get; set; }
+        
+
+        public Categoria(string nombre)
+        {
+
+            if (nombre.Length < 3 || nombre.Length > 15)
+            {
+                throw new ExcepcionLargoTexto();
+
+            }
+
+            this.Nombre = nombre;
+
+        }
+    }
+}
---
+++ Obligatorio/PruebasUnitarias/PruebasCategoria.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Negocio;
+using Negocio.Excepciones;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class PruebasCategoria
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ExcepcionLargoTexto))]
+        public void NoSePuedeCrearUnaCategoriaConNombreMenor3Caracteres()
+        {
+            Categoria nuevaCategoria = new Categoria("12");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExcepcionLargoTexto))]
+        public void NoSePuedeCrearUnaCategoriaConNombreMayor15Caracteres()
+        {
+            Categoria nuevaCategoria = new Categoria("1234512345123451");
+
+        }
+
+        [TestMethod]
+        public void SeGuardaCorrectamenteElNombre()
+        {
+            Categoria nuevaCategoria = new Categoria("hola");
+            string nombre = "hola";
+            Assert.AreEqual(nombre, nuevaCategoria.Nombre);
+        }
+
+
+
+
+    }
+}
