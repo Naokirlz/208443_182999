4c8064481c4db41647ce7ec1049550d841637229
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2021-04-20 17:10:36 -0300

    Pruebas: pruebas e implenetación sobre el sitio

---
+++ Obligatorio/Negocio/Clases/Contrasenia.cs
@@ -0,0 +1,29 @@
+﻿using Negocio.Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Clases
+{
+    public class Contrasenia
+    {
+        public string Sitio { get; }
+        public string Usuario { get; }
+
+        public Contrasenia() { }
+        public Contrasenia(string unSitio) : base()
+        {
+            if (unSitio.Length < 3) throw new ExcepcionLargoTexto();
+            if (unSitio.Length > 25) throw new ExcepcionLargoTexto();
+            this.Sitio = unSitio;
+        }
+
+        public Contrasenia(string unSitio, string unUsuario) : this(unSitio)
+        {
+            if (unUsuario.Length < 5) throw new ExcepcionLargoTexto();
+            this.Usuario = unUsuario;
+        }
+    }
+}
---
+++ Obligatorio/PruebasUnitarias/Contrasena/PruebasContrasena.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Negocio.Clases;
+using Negocio.Excepciones;
+
+namespace PruebasUnitarias.Contrasena
+{
+    [TestClass]
+    public class PruebasContrasena
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ExcepcionLargoTexto))]
+        public void NoSePuedeCrearUnaContrasenaConSitioMenor3Caracteres()
+        {
+            Contrasenia nuevaContrasenia = new Contrasenia("12");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExcepcionLargoTexto))]
+        public void NoSePuedeCrearUnaContrasenaConSitioMayor25Caracteres()
+        {
+            Contrasenia nuevaContrasenia = new Contrasenia("12345123451234512345123451");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExcepcionLargoTexto))]
+        public void NoSePuedeCrearUnaContrasenaConUsuarioMenor5Caracteres()
+        {
+            Contrasenia nuevaContrasenia = new Contrasenia("Sitio", "1234");
+        }
+    }
+}
