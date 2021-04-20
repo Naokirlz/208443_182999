e60fcacf5a81330f4ae70e79aae1221b6f708932
Naokirlz <cristian_pd88@hotmail.com>
2021-04-20 18:07:09 -0300

    Funcionalidad: Tarjeta Credito

---
+++ Obligatorio/Negocio/Clases/TarjetaCredito.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Clases
+{
+    public class TarjetaCredito
+    {
+        private static List<TarjetaCredito> tarjetas = new List<TarjetaCredito>();
+
+        public Categoria Categoria { get; set; }
+        public string Nombre { get; set; }
+        // ver si despues el tipo conviene que sea una clase
+        public string Tipo { get; set; }
+        public string Numero { get; set; }
+        public int Codigo { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public String Nota { get; set; }
+
+
+        public static TarjetaCredito Alta(TarjetaCredito nueva)
+        {
+            // hago los controles
+            tarjetas.Add(nueva);
+            // si agrego y si no tiro exception
+            return nueva;
+
+        }
+
+    }
+}
---
+++ Obligatorio/PruebasUnitarias/PruebasTarjetaCredito.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Negocio;
+using Negocio.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class PruebasTarjetaCredito
+    {
+        //private GestorCategorias Gestor = new GestorCategorias();
+        [TestMethod]
+        
+        public void AltaTarjetaDeCredito()
+        {
+            TarjetaCredito nueva = new TarjetaCredito();
+
+            Categoria categoria = new Categoria("Dummy");
+            string nombreTarjeta = "PruebaNombre";
+            string tipoTarjeta = "PruebaTipo";
+            string numeroTarjeta = "1234-1234-1234-1234";
+            int codigoTarjeta = 123;
+            DateTime vencimientoTarjeta = DateTime.Now;
+            string notaOpcionalTarjeta = "Nota Opcional";
+
+            nueva.Categoria = categoria;
+            nueva.Nombre = nombreTarjeta;
+            nueva.Tipo = tipoTarjeta;
+            nueva.Numero = numeroTarjeta;
+            nueva.Codigo = codigoTarjeta;
+            nueva.Vencimiento = vencimientoTarjeta;
+            nueva.Nota = notaOpcionalTarjeta;
+
+
+
+        }
+
+
+    }
+}
