d88ce5deb5bfbed796dae81262da191e4b31f4bc
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2021-04-25 17:03:58 -0300

    Interfaz: Ventana gestión de contraseñas

---
+++ Obligatorio/Interfaz/Contrasenias/GestionContrasenias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz.Contrasenias
+{
+    public partial class GestionContrasenias : UserControl
+    {
+        public GestionContrasenias()
+        {
+            InitializeComponent();
+        }
+    }
+}
