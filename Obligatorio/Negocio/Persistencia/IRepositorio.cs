791b92bf95ffcf6a3b69c59cb3c307b31a169aca
Naokirlz <cristian_pd88@hotmail.com>
2021-06-06 17:39:03 -0300

    Refactor: Se Crea Interfaz <<IRepositorio>>
    Se aplican técnicas SOLID para poder reemplazar el repositorio en memoria

---
+++ Obligatorio/Negocio/Persistencia/IRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Persistencia
+{
+    public interface IRepositorio<T>
+    {
+
+        //T Get(int id);
+        T Buscar(T entity);
+        IEnumerable<T> ObtenerTodas();
+        int Alta(T entity);
+        void Baja(T entity);
+        void Modificar(T entity);
+        void Existe(T entity);
+
+    }
+}
