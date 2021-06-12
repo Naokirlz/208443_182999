9790df26a5ecde174545368fbdf027ee2f92c1c6
Naokirlz <cristian_pd88@hotmail.com>
2021-06-12 12:27:58 -0300

    Pruebas: se crea prueba unitaria FuenteArchivo

---
+++ Obligatorio/Negocio/Utilidades/FuenteArchivo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Utilidades
+{
+    public class FuenteArchivo : IFuente
+    {
+        public int BuscarPasswordOContraseniaEnFuente(string buscado)
+        {
+            int cantidad = 0;
+            string ruta = AppDomain.CurrentDomain.BaseDirectory + "\\Archivos";
+            List<string> strFiles = Directory.GetFiles(ruta, "*", SearchOption.AllDirectories).ToList();
+
+            foreach (string fichero in strFiles)
+            {
+                List<string> lineas = File.ReadAllLines(fichero).ToList();
+                foreach (var item in lineas)
+                {
+                    if (item.Equals(buscado)) cantidad++;
+                    
+                }
+            }
+            return cantidad;
+        }
+
+        public void CrearDataBreach(string dataBreach)
+        {
+            string[] rutaDetalle = dataBreach.Split('\\').ToArray();
+            string nombrearchivo = rutaDetalle[rutaDetalle.Length - 1];
+            string ruta = AppDomain.CurrentDomain.BaseDirectory + "\\Archivos\\" + nombrearchivo;
+            System.IO.File.Copy(dataBreach, ruta, true);
+        }
+    }
+}
--- Obligatorio/PruebasUnitarias/PruebasFuenteArchivo.cs
+++ Obligatorio/PruebasUnitarias/PruebasFuenteArchivo.cs
@@ -1,15 +1,55 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Negocio.Utilidades;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace PruebasUnitarias
 {
     [TestClass]
     public class PruebasFuenteArchivo
     {
+
+        [TestInitialize]
+        public void InicializarPruebas()
+        {
+            string ruta = AppDomain.CurrentDomain.BaseDirectory + "\\Archivos";
+            List<string> strFiles = Directory.GetFiles(ruta, "*", SearchOption.AllDirectories).ToList();
+
+            foreach (string fichero in strFiles)
+            {
+                File.Delete(fichero);
+            }
+        }
+
+
         [TestMethod]
         public void SePuedeCopiarUnArchivoDeFileSystemALaCarpetaDeFuentes()
         {
+            string ruta= AppDomain.CurrentDomain.BaseDirectory + "\\ArchivosOriginales\\Fuente.txt";
+            string rutaDestino = AppDomain.CurrentDomain.BaseDirectory + "\\Archivos\\Fuente.txt";
+            FuenteArchivo nueva = new FuenteArchivo();
+            nueva.CrearDataBreach(ruta);
+            Assert.IsTrue(File.Exists(rutaDestino));
+    
+        }
+
+        [TestMethod]
+        public void SePuedeLeerUnArchivo()
+        {
+            FuenteArchivo nueva = new FuenteArchivo();
+            string rutaArchivo = AppDomain.CurrentDomain.BaseDirectory + "\\ArchivosOriginales\\Fuente.txt";
+            nueva.CrearDataBreach(rutaArchivo);
+            int cantidad = nueva.BuscarPasswordOContraseniaEnFuente("55555");
+            Assert.IsTrue(cantidad > 0);
+            
+
+
 
         }
+
+
+
     }
 }
